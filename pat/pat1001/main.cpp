615110fcadcf42405620d23e9a9087f781d6485f
Rico-nan <jn0792@qq.com>
2019-01-03 08:41:46 +0800

    ac pat1001

--- pat/pat1001/main.cpp
+++ pat/pat1001/main.cpp
@@ -5,11 +5,18 @@ using namespace std;
 int main()
 {
     int n;
+    long long int a,b,c;
     cin>>n;
-
-
-
-    cout << "Hello world!" << endl;
-
+for(int i=1;i<=n;i++){
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if(a+b>c){
+        cout<<"Case #"<<i<<": true"<<endl;
+    }
+    else{
+        cout<<"Case #"<<i<<": false"<<endl;
+    }
+}
     return 0;
 }
