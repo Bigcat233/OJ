6d364ed9a29924e2124a73b4f71f2c3762575abb
Rico-nan <jn0792@qq.com>
2019-01-04 10:34:34 +0800

    changes in pat1002

--- pat/pat1002/main.cpp
+++ pat/pat1002/main.cpp
@@ -6,8 +6,21 @@ int main()
 {
     int n;
     cin>>n;
-    int digital[n];
+    int digital[n],A1[n],A2[n],A3[n],A4[n],A0[n];
+    int a1=0;
+    int a2=0;
+    int a3=0;
+    int a4=0;
+    int a0=0;
     for(int i=0;i<n;i++){
+        switch(digital[i]%5){
+        case 0:
+            A0[a0]=digital[i];
+        case 1:;
+        case 2:;
+        case 3:;
+        case 4:;
 
+        }
     }
 }
