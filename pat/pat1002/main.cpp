eb879ea2d501949536f809c834a6bf61a9cacbb7
Rico-nan <jn0792@qq.com>
2019-01-09 14:42:21 +0800

    changed pat1002

--- pat/pat1002/main.cpp
+++ pat/pat1002/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     int n;
+    int result1,result2,resulut3,reslut5;
     cin>>n;
     int digital[n],A1[n],A2[n],A3[n],A4[n],A0[n];
     int a1=0;
@@ -16,11 +17,24 @@ int main()
         switch(digital[i]%5){
         case 0:
             A0[a0]=digital[i];
-        case 1:;
-        case 2:;
-        case 3:;
-        case 4:;
-
+            a0++;break;
+        case 1:
+            A1[a1]=digital[i];
+            a1++;break;
+        case 2:
+            A2[a2]=digital[i];
+            a2++;break;
+        case 3:
+            A3[a3]=digital[i];
+            a3++;break;
+        case 4:
+            A4[a4]=digital[i];
+            a4++;break;
+        default:break;
         }
     }
+    for(int i =0;i<a0;i++){
+
+    }
+
 }
