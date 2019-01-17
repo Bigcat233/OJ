95d8c8e3cc29cb44f858a6ec4c1f00bef2ae0e38
Rico-nan <jn0792@qq.com>
2019-01-17 13:52:31 +0800

    finish pat1003

--- pat/pat1003/main.cpp
+++ pat/pat1003/main.cpp
@@ -3,39 +3,43 @@
 using namespace std;
 
 bool issushu(int a);
-void make_bool_set();
-bool sushu[10001];
+
 int main()
 {
-    make_bool_set();
     int low,high;
-    int count_10=1;
     cin>>low;
     cin>>high;
-    for(int i=low;i<=high;i++){
-        if(sushu[i]){
-            if(count_10==10){
-                cout<<i<<endl;
-                count_10=1;
-            }
-            else{
-                cout<<i<<" ";
-                count_10++;
-            }
+    int count_num=0;
+    int count_10=1;
+    int i=2;
+    while(count_num<high){
+       while(count_num<low-1){
+            if(issushu(i))count_num++;
+            i++;
+       }
+       if(issushu(i)){
+        if(count_10==10){
+            cout<<i<<endl;
+            count_10=1;
         }
+        else{
+            cout<<i;
+            if(!(count_num==high-1)){
+                cout<<" ";
+            }
+            count_10++;
+        }
+       count_num++;
+       }
+       i++;
     }
     return 0;
 }
 
 bool issushu(int a){
-for(int j=2;j<sqrt(a);j++){
+for(int j=2;j<=sqrt(a);j++){
     if((a%j)==0)return false;
 }
 return true;
 }
 
-void make_bool_set(){
-for(int i=0;i<=10000;i++){
-    sushu[i]=issushu(i);
-}
-}
