26ed77708d7a20b1f27cba5f9f7cfaa6c29de7d3
Rico-nan <jn0792@qq.com>
2019-01-22 22:56:23 +0800

    postive interger error

--- pat/adlpat1001/main.cpp
+++ pat/adlpat1001/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
-#include <vector> // TO DO:learn vector
+#include <sstream>
 using namespace std;
-void dfs(int start,int index,int end);
-
+long long int a;
+long long int b;
 int main()
 {
-
+    cin>>a;
+    cin>>b;
+    stringstream sstr;
+    sstr << a+b;
+    string c = sstr.str();
+    int p=0;
+    int count_num=0;
+    int length=c.length();
+    if(c[0]=='-'){
+            cout<<'-';
+            p++;
+            length--;}
+    for(p;p<=(length%3);p++){
+        cout<<c[p];
+    }
+    for(p;p<=length;p++){
+        if(count_num==3){
+            cout<<',';
+            cout<<c[p];
+            count_num=0;
+        }
+        else{
+            cout<<c[p];
+            count_num++;
+        }
+    }
     return 0;
-}
-
-
-void dfs(int start,int index,int end){
-
-
 }
