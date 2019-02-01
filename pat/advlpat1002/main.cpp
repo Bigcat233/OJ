c680b0aea24db52244defd2e6fb2d68d815700a6
Rico-nan <jn0792@qq.com>
2019-02-01 23:19:07 +0800

    update 1002

--- pat/advlpat1002/main.cpp
+++ pat/advlpat1002/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<double> a(1001,0);
+    vector<double> b(1001,0);//下标记为指数 数值记为对应数值
+
+
     return 0;
 }
